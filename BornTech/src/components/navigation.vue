2ef7792bc9a833150b62d0c076ec186bb9086f67
DevadsFelipe <devads.felipe@gmail.com>
2023-10-15 18:08:37 -0300

    home building container

--- BornTech/src/components/navigation.vue
+++ BornTech/src/components/navigation.vue
@@ -10,11 +10,15 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+        <div class="navbar-nav ms-auto">
           <router-link to="/" class="nav-link">Home</router-link>
           <router-link to="/about" class="nav-link">About</router-link>
           <router-link to="/skills" class="nav-link">Skills</router-link>
           <router-link to="/contact" class="nav-link">Contact</router-link>
+        </div>
+        <div class="navbar-nav ms-auto">
+            <a href="https://www.linkedin.com/in/felipe-o-612b4b84" target="_blank"><i class="fab fa-linkedin nav-link"></i></a>
+            <a href="https://www.instagram.com/felipe_ol_/" target="_blank"><i class="fab fa-instagram nav-link"></i></a>
         </div>
       </div>
     </div>
--- BornTech/src/views/home.vue
+++ BornTech/src/views/home.vue
@@ -2,7 +2,31 @@
 </script>
 
 <template>
-    <h2>Home</h2>
+    <div class="container-fluid">
+        <div>Olá,</div>
+        <div></div>
+        <div>Sou o Felipe e vou te ajudar com seu próximo projeto</div>
+        <div class="photo-me">
+            <img src="../assets/photo-me.png" class="img-fluid" alt="me">
+        </div>
+        <div>
+            <button class="downloadCv-button">Download CV</button>
+            <button>Vamos bater um papo</button>
+        </div>
+
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+
+.photo-me{
+    display: inline-flex;
+    justify-content: left;
+    height: 10%;
+};
+
+.downloadCv-button{
+
+};
+
+</style>
