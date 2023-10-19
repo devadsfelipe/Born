111b57be933107de5621fc728811543dba11f6c0
DevadsFelipe <devads.felipe@gmail.com>
2023-10-18 23:20:42 -0300

    Add image model into home page

--- BornTech/src/views/home.vue
+++ BornTech/src/views/home.vue
@@ -6,27 +6,29 @@
     <div class="row">
         <div class="cols textdata">
             <h2>Olá, sou o <span>Felipe</span></h2>
-            <h3>Full Stack Developer</h3>
-            <p>Sou fascinado em <b>solucionar problemas</b> complexos e <b>prospectar soluções</b> inovadoras com
-                uso da
-                tecnologia. Desbravador de otimizações de recursos financeiros através da <b>Cloud Computing</b>
-                para
-                chegar o mais perto possível do equilibrio entre custo/operação
-            </p>
-            <div class="btn">
-                <button>Download CV</button>
+            <div style="margin-left: 15px;">
+                <h3>Full Stack Developer</h3>
+                <p>Sou fascinado em <b>solucionar problemas</b> complexos e <b>prospectar soluções</b> inovadoras com
+                    uso da
+                    tecnologia. Desbravador de otimizações de recursos financeiros através da <b>Cloud Computing</b>
+                    para
+                    chegar o mais perto possível do equilibrio entre custo/operação
+                </p>
+                <div class="btn">
+                    <button>Download CV</button>
+                </div>
+                <div class="socialmidia">
+                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-github"></i>
+                </div>
             </div>
-            <div class="socialmidia">
-                <i class="fab fa-linkedin"></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-github"></i>
+        </div>
+        <div class="cols imagedata">
+            <div class="imgbox">
+                <img src="../assets/photo-me.png">
             </div>
-            <!-- <div class="cols imagedata">
-                    <div class="imgbox">
-                        <img src="../assets/photo-me.png">
-                    </div>
-                    <div class="pattern"></div>
-                </div> -->
+            <div class="pattern"></div>
         </div>
     </div>
 </template>
@@ -36,7 +38,7 @@
     position: relative;
     display: flex;
     width: 100%;
-    margin: 7%;
+    margin: 2%;
 }
 
 .cols {
@@ -53,7 +55,7 @@
 .textdata h2 {
     font-family: 'Tilt Neon', sans-serif;
     color: var(--textcolor);
-    font-size: 5em;
+    font-size: 4em;
     font-weight: 400;
     /* z-index: 1111; */
 }
@@ -72,7 +74,7 @@
 }
 
 .textdata p {
-    font-size: 18px;
+    font-size: 1em;
     font-weight: 400;
     margin: 20px 0;
     color: var(--textcolor);
@@ -85,30 +87,32 @@
 
 .textdata .btn {
     width: 100%;
-    margin: 20px 0px;
+    margin: 20px 0px 130px;
     padding: 0px;
     text-align: start;
+    min-width: 170px;
 }
 
 .btn button {
     border: none;
+    min-width: 60px;
     background: var(--primary);
     color: var(--secondary);
     font-size: 15px;
     text-transform: capitalize;
     letter-spacing: 1px;
     padding: 10px 30px;
-    box-shadow: 0 10px 10px 0 var(--textcolor);
+    box-shadow: 0 10px 20px 0 var(--textcolor);
     cursor: pointer;
 }
 
 .btn button:hover {
     transition: 0.4s ease;
-    padding: 10px 40px;
+    padding: 10px 90px;
 }
 
 .socialmidia {
-    margin: 20px 0px;
+    /* margin: 20px 0px; */
 }
 
 .socialmidia i {
@@ -129,19 +133,16 @@
 }
 
 .imgbox {
-    position: relative;
-    width: 800;
-    top: -50px;
     z-index: 1;
-    transition: 0.3s ease;
+    transition: 0.9s ease;
 }
 
 .imgbox img {
-    width: 100%;
+    width: 70%;
 }
 
 .imagedata:hover .imgbox {
-    transform: translateX(-10px);
+    transform: translateX(10px);
 }
 
 .imagedata .pattern {
